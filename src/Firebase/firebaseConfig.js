0f3c575dc0ab78fd25672d21a5c3d4fd025d6ea5
developerMasum <developer.masum2001@gmail.com>
2023-07-24 22:05:12 +0600

    file added

---
+++ src/Firebase/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBC75BZWypa_DJ4MvhIVUOrfVStgbP6ZOs",
+  authDomain: "collegelist-535cc.firebaseapp.com",
+  projectId: "collegelist-535cc",
+  storageBucket: "collegelist-535cc.appspot.com",
+  messagingSenderId: "585830636292",
+  appId: "1:585830636292:web:0ac2f2b9dd64ad98d3ce51"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
---
+++ src/components/Feedback/FeedBackForm.jsx
@@ -0,0 +1,128 @@
+import axios from 'axios';
+import React, { useContext } from 'react';
+import { useForm, Controller } from 'react-hook-form';
+import Swal from 'sweetalert2';
+import { AuthContext } from '../../providers/AuthProvider';
+import { useNavigate } from 'react-router-dom';
+
+const ReviewForm = () => {
+  const navigate = useNavigate()
+  const {user} = useContext(AuthContext)
+
+  const { handleSubmit, control, setValue } = useForm();
+
+  const onSubmit = (data) => {
+
+    console.log(data);
+    const newItem = {
+      avatar:user?.photoURL,
+      name:data.review,
+      rating: data.rating
+      
+    };
+    console.log('new',newItem);
+
+
+    axios
+    .post('http://localhost:5000/feedback', newItem)
+    .then((response) => {
+      const { data } = response;
+      if (data.insertedId) {
+        // Reset the form (if needed)
+        // reset();
+
+        Swal.fire({
+          position: 'center',
+          icon: 'success',
+          title: 'Your Application has been sent successfully',
+          showConfirmButton: false,
+          timer: 1500,
+        });
+
+        // Navigate to the desired page (e.g., "/my-college")
+        navigate('/')
+      }
+    })
+    .catch((error) => {
+      // Handle error if necessary
+      console.error('Error submitting application:', error);
+    });
+
+
+
+   
+  };
+
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <form
+        className="w-full max-w-lg bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <h2 className="text-2xl font-semibold mb-6">Write a Review</h2>
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="review"
+          >
+            Review
+          </label>
+          <Controller
+            name="review"
+            control={control}
+            defaultValue=""
+            render={({ field }) => (
+              <textarea
+                {...field}
+                id="review"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                rows="4"
+                placeholder="Write your review here..."
+              />
+            )}
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="rating"
+          >
+            Rating
+          </label>
+          <Controller
+            name="rating"
+            control={control}
+            defaultValue=""
+            render={({ field }) => (
+              <input
+                {...field}
+                id="rating"
+                type="number"
+                min="1"
+                max="5"
+                step="1"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                placeholder="Enter your rating (1-5)"
+                onChange={(e) => setValue('rating', parseInt(e.target.value))}
+              />
+            )}
+          />
+        </div>
+        <div className="flex items-center justify-between">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            Submit Review
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default ReviewForm;
+
+
+
+// make a ReviewForm using react hook form Review input and rating input , make it by tailwindcss and jsx.  style it beautiful and responsive
